ab5f1a7923c2102ec4128256d2f06a45054a1ed1
Erick Sugahara <erick.sugahara@softvaro.com.br>
2020-11-13 19:24:47 -0300

    change validators

--- src/containers/Register/index.tsx
+++ src/containers/Register/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { debounce } from 'lodash';
 
 import Panel from '../../components/Panel';
@@ -6,8 +6,8 @@ import Button from '../../components/Button';
 import Header from '../../components/Header';
 import Input from '../../components/Input';
 import { Container } from './styles';
-import { validateEmail } from '../../utils/validators';
-import { maskCnpj } from '../../utils/masks';
+import { validateEmail, validateCnpj, validateAccount, validateAgency } from '../../utils/validators';
+import { maskCnpj, maskPhone, maskAgency, maskAccount } from '../../utils/masks';
 
 const Register: React.FC = () => {
   const [name, setName] = useState('');
@@ -30,27 +30,79 @@ const Register: React.FC = () => {
 
   const [state, setState] = useState('');
 
-  const debounceEmailCheckIsValid = useCallback(
+  const [isValidAgency, setIsValidAgency] = useState(true);
+  const [agency, setAgency] = useState('');
+
+  const [isValidAccount, setIsValidAccount] = useState(true);
+  const [account, setAccount] = useState('');
+
+  const debounceNameCheckIsValid = useCallback(
     debounce(async (st) => {
-      setIsValidName(validateEmail(st));
+      if (!st) {
+        setIsValidName(false);
+        return;
+      }
+      setIsValidName(true);
     }, 1000),
     [validateEmail, debounce],
+  );
+
+  const debounceEmailCheckIsValid = useCallback(
+    debounce(async (st) => {
+      if (!st) {
+        setIsValidEmail(true);
+        return;
+      }
+      setIsValidEmail(validateEmail(st));
+    }, 1000),
+    [validateEmail, debounce],
+  );
+
+  const debounceCnpjCheckIsValid = useCallback(
+    debounce(async (st) => {
+      setIsValidCnpj(validateCnpj(st));
+    }, 1000),
+    [validateCnpj, debounce],
+  );
+
+  const debounceAccountCheckIsValid = useCallback(
+    debounce(async (st) => {
+      if (!st) {
+        setIsValidAccount(true);
+        return;
+      }
+      setIsValidAccount(validateAccount(st));
+    }, 1000),
+    [validateAccount, debounce],
+  );
+
+  const debounceAgencyCheckIsValid = useCallback(
+    debounce(async (st) => {
+      if (!st) {
+        setIsValidAgency(true);
+        return;
+      }
+      setIsValidAgency(validateAgency(st));
+    }, 1000),
+    [validateAgency, debounce],
   );
 
   const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     switch (e.target.id) {
       case 'inp_name':
         setName(e.target.value);
-        debounceEmailCheckIsValid(e.target.value);
+        debounceNameCheckIsValid(e.target.value);
         break;
       case 'inp_fantasy_name':
         setFantasyName(e.target.value);
         break;
       case 'inp_cnpj':
         setCnpj(maskCnpj(e.target.value));
+        debounceCnpjCheckIsValid(e.target.value);
         break;
       case 'inp_email':
         setEmail(e.target.value);
+        debounceEmailCheckIsValid(e.target.value);
         break;
       case 'inp_address':
         setAddress(e.target.value);
@@ -62,7 +114,15 @@ const Register: React.FC = () => {
         setState(e.target.value);
         break;
       case 'inp_phone':
-        setPhone(e.target.value);
+        setPhone(maskPhone(e.target.value));
+        break;
+      case 'inp_agency':
+        setAgency(maskAgency(e.target.value));
+        debounceAgencyCheckIsValid(maskAgency(e.target.value));
+        break;
+      case 'inp_account':
+        setAccount(maskAccount(e.target.value));
+        debounceAccountCheckIsValid(maskAccount(e.target.value));
         break;
       default:
     }
@@ -82,7 +142,14 @@ const Register: React.FC = () => {
             value={name}
             onChange={handleChange}
           />
-          <Input id="inp_fantasy_name" type="text" label="Nome Fantasia:" maxLength={100} />
+          <Input
+            id="inp_fantasy_name"
+            type="text"
+            label="Nome Fantasia:"
+            maxLength={100}
+            value={email}
+            onChange={handleChange}
+          />
           <Input
             id="inp_cnpj"
             type="text"
@@ -92,11 +159,51 @@ const Register: React.FC = () => {
             value={cnpj}
             onChange={handleChange}
           />
-          <Input id="inp_email" type="text" label="E-email:" isInvalid={!isValidEmail} maxLength={100} />
-          <Input id="inp_address" type="text" label="Endereço:" maxLength={100} />
-          <Input id="inp_city" type="text" label="Cidade:" maxLength={100} />
-          <Input id="inp_state" type="text" label="Estado:" maxLength={100} />
-          <Input id="inp_phone" type="text" label="Telefone:" isInvalid={!isValidPhone} maxLength={15} />
+          <Input
+            id="inp_email"
+            type="text"
+            label="E-email:"
+            isInvalid={!isValidEmail}
+            maxLength={100}
+            onChange={handleChange}
+          />
+          <Input
+            id="inp_address"
+            type="text"
+            label="Endereço:"
+            maxLength={100}
+            value={address}
+            onChange={handleChange}
+          />
+          <Input id="inp_city" type="text" label="Cidade:" maxLength={100} value={city} onChange={handleChange} />
+          <Input id="inp_state" type="text" label="Estado:" maxLength={100} value={state} onChange={handleChange} />
+          <Input
+            id="inp_phone"
+            type="text"
+            label="Telefone:"
+            isInvalid={!isValidPhone}
+            maxLength={15}
+            value={phone}
+            onChange={handleChange}
+          />
+          <Input
+            id="inp_agency"
+            type="text"
+            label="Agência:"
+            isInvalid={!isValidAgency}
+            maxLength={5}
+            value={agency}
+            onChange={handleChange}
+          />
+          <Input
+            id="inp_account"
+            type="text"
+            label="Conta:"
+            isInvalid={!isValidAccount}
+            maxLength={8}
+            value={account}
+            onChange={handleChange}
+          />
           <Button
             label="Enviar"
             onClick={() => {
--- src/utils/masks.ts
+++ src/utils/masks.ts
@@ -1,4 +1,33 @@
 export const maskCnpj = (cnpj: string) => {
   if (!cnpj) return cnpj;
-  return cnpj.replace(/\D/g, '').replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5');
+
+  let formatted = cnpj.replace(/\D/g, '');
+  formatted = formatted.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d)/g, '$1.$2.$3/$4-$5');
+  formatted = formatted.replace(/^(\d{2})(\d{3})(\d{3})(\d)/g, '$1.$2.$3/$4');
+  formatted = formatted.replace(/^(\d{2})(\d{3})(\d)/g, '$1.$2.$3');
+  formatted = formatted.replace(/^(\d{2})(\d)/g, '$1.$2');
+  return formatted;
+};
+
+export const maskPhone = (tel: string) => {
+  if (!tel) return tel;
+  let formatted = tel.replace(/\D/g, '');
+  formatted = formatted.replace(/^(\d{2})(\d)/g, '($1) $2');
+  formatted = formatted.replace(/(\d)(\d{4})$/, '$1-$2');
+  return formatted;
+};
+
+export const maskAgency = (agency: string) => {
+  if (!agency) return agency;
+  let formatted = agency.replace(/\D/g, '');
+  formatted = formatted.replace(/^(\d{3})(\d)/g, '$1-$2');
+  return formatted;
+};
+
+export const maskAccount = (account: string) => {
+  if (!account) return account;
+  let formatted = account.replace(/\D/g, '');
+  formatted = formatted.replace(/^(\d{2})(\d{3})(\d)/g, '$1.$2-$3');
+  formatted = formatted.replace(/^(\d{2})(\d)/g, '$1.$2');
+  return formatted;
 };
